e3d3bd2a8a8896d3551c1a4c4795feeb123239e1
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-11-22 07:09:20 +0000

    [add]session機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  def require_user_signed_in
+    unless signed_in?
+      redirect_to login_path
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
 end
